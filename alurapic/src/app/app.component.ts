498cb627592b98b198e6afcb6f40432db512dcb1
@lucas.felipe <l17.lucas@gmail.com>
2021-07-30 12:08:47 -0300

    add api

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'alurapic';
 
+  photos=[
+  {url:"https://static.wikia.nocookie.net/watchdogscombined/images/9/99/Wrench_WD2_Render.png.png", description:"wrench"},
+  {url:"https://i.ytimg.com/vi/_Tx6mar2kwc/maxresdefault.jpg", description:"wrench2"}]
+
 }
