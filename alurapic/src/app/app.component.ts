89729acaa6fea7cd08b03f7a8b6a8e70b53fbe07
@lucas.felipe <l17.lucas@gmail.com>
2021-08-02 09:09:05 -0300

    add componentes

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -1,22 +1,10 @@
-import { PhotoService } from './photos/photo/photo.service';
 import { Component, OnInit } from '@angular/core';
-import { Photo } from './photos/photo/photo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent{
   title = 'alurapic';
-
-  photos: Photo[] = [];
-
-  constructor(private photoService: PhotoService) {}
-
-  ngOnInit(): void {
-    this.photoService
-      .listFromUser('flavio')
-      .subscribe((photos) => (this.photos = photos));
-  }
 }
