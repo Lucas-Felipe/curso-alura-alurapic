ca542bee38fa4b8fc822645d2f3a3b777b94cc0f
@lucas.felipe <l17.lucas@gmail.com>
2021-08-02 10:42:57 -0300

    pagina de erro, organização do codigo

---
+++ alurapic/src/app/photos/photo-list/photos/photos.component.ts
@@ -0,0 +1,28 @@
+import { Photo } from './../../photo/photo';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photos',
+  templateUrl: './photos.component.html',
+  styleUrls: ['./photos.component.css'],
+})
+export class PhotosComponent implements OnInit {
+  @Input() photos: Photo[] = [];
+  rows: any[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.rows = this.groupColumns(this.photos);
+  }
+
+  groupColumns(photos: Photo[]) {
+    const newRows = [];
+
+    for (let index = 0; index < photos.length; index += 3) {
+      newRows.push(photos.slice(index, index + 3));
+    }
+
+    return newRows;
+  }
+}
