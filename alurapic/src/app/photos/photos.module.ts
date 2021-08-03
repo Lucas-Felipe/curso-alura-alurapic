f8168035dd35983ca7414ef1383e330a8684ec04
@lucas.felipe <l17.lucas@gmail.com>
2021-08-03 12:54:34 -0300

    colocando módulos

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -1,16 +1,9 @@
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoModule } from './photo/photo.module';
 import { NgModule } from '@angular/core';
 
-import { PhotoComponent } from './../photos/photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
-
 @NgModule({
-  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent,FilterByDescription, LoadButtonComponent],
-  imports: [HttpClientModule, CommonModule],
+  imports: [PhotoModule,PhotoFormModule,PhotoListModule],
 })
 export class PhotosModule {}
